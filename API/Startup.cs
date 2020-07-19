58ad8958d73895cfc55efc6f4690337b2152d73f
keith <xqk559@gmail.com>
2020-07-19 07:46:01 -0400

    Replaced ProductRepository with GenericRepository

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,6 +25,7 @@ namespace API
       services.AddDbContext<StoreContext>(x =>
           x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
       services.AddScoped<IProductRepository, ProductRepository>();
+      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
