701818f7567d89316dfd046b82d4a91cc5e83073
keith <xqk559@gmail.com>
2020-07-22 10:54:31 -0400

    Added AutoMapper to map Dtos to ProductsController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
@@ -18,8 +19,14 @@ namespace API.Controllers
     private readonly IGenericRepository<ProductBrand> _productBrandRepo;
     private readonly IGenericRepository<ProductType> _productTypeRepo;
     private readonly IGenericRepository<Product> _productsRepo;
-    public ProductsController(IGenericRepository<Product> productsRepo, IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType> productTypeRepo)
+
+    private readonly IMapper _mapper;
+    public ProductsController(IGenericRepository<Product> productsRepo,
+    IGenericRepository<ProductBrand> productBrandRepo,
+    IGenericRepository<ProductType> productTypeRepo,
+    IMapper mapper)
     {
+      _mapper = mapper;
       _productsRepo = productsRepo;
       _productTypeRepo = productTypeRepo;
       _productBrandRepo = productBrandRepo;
@@ -51,16 +58,7 @@ namespace API.Controllers
 
       var product = await _productsRepo.GetEntityWithSpec(spec);
 
-      return new ProductToReturnDto
-      {
-        Id = product.Id,
-        Name = product.Name,
-        Description = product.Description,
-        PictureUrl = product.PictureUrl,
-        Price = product.Price,
-        ProductBrand = product.ProductBrand.Name,
-        ProductType = product.ProductType.Name
-      };
+      return _mapper.Map<Product, ProductToReturnDto>(product);
     }
 
     [HttpGet("brands")]
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure;
 using Infrastructure.Data;
@@ -26,6 +28,7 @@ namespace API
           x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
       services.AddScoped<IProductRepository, ProductRepository>();
       services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+      services.AddAutoMapper(typeof(MappingProfiles));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
