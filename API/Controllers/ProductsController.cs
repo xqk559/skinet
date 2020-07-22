dfabf404cbff8409363e0a43e295b5520ec43050
keith <xqk559@gmail.com>
2020-07-22 11:12:22 -0400

    AddedProductUrlResolver to give full URLs for images on API server

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,18 +1,15 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [ApiController]
+    [ApiController]
   [Route("api/[controller]")]
   public class ProductsController : ControllerBase
   {
