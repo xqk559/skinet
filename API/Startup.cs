c6cd18432ec1df1d8e44790a2149375844ae3c7f
keith <xqk559@gmail.com>
2020-07-27 08:08:00 -0400

    Added Extensions to make seperate files for services and middleware in startup.cs to clean it up

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,13 @@
-using System.Linq;
-using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -29,30 +25,9 @@ namespace API
       services.AddControllers();
       services.AddDbContext<StoreContext>(x =>
           x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-      services.AddScoped<IProductRepository, ProductRepository>();
-      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
       services.AddAutoMapper(typeof(MappingProfiles));
-      services.Configure<ApiBehaviorOptions>(options =>
-      {
-        options.InvalidModelStateResponseFactory = ActionContext =>
-        {
-            var errors = ActionContext.ModelState
-                .Where(e => e.Value.Errors.Count > 0)
-                .SelectMany(x => x.Value.Errors)
-                .Select(x => x.ErrorMessage).ToArray();
-
-            var errorResponse = new ApiValidationErrorResponse
-            {
-                Errors = errors
-            };
-
-            return new BadRequestObjectResult(errorResponse);
-        };
-      });
-      services.AddSwaggerGen(c =>
-          {
-              c.SwaggerDoc("v1", new OpenApiInfo {Title = "Skinet API", Version = "v1"});
-          });
+      services.AddApplicationServices();
+      services.AddSwaggerDocumentation();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,9 +42,7 @@ namespace API
       app.UseStaticFiles();
 
       app.UseAuthorization();
-      app.UseSwagger();
-      app.UseSwaggerUI(c =>
-        {c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1");});
+      app.UseSwaggerDocumentation();
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
