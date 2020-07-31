b1c8521ab1429a75f4cdb04e0bb6dfe7db74a97d
keith <xqk559@gmail.com>
2020-07-31 11:21:52 -0400

    Added pagination and product typescript interfaces in app.component.ts to ensure they are typse safe

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
-      this.products = response.data;
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
     }, error => {
       console.log(error);
     });
