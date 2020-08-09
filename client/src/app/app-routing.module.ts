6eaed7a40cbb7d768b7922757e5247a4b5fb6aed
keith <xqk559@gmail.com>
2020-08-09 11:30:25 -0400

    Built test-error component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 import { HomeComponent } from './home/home.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
