4d2250bff93a00bd839bbf52c8971c2bfa11398a
keith <xqk559@gmail.com>
2020-08-09 10:19:05 -0400

    Product details page now linked to individual product names by id

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+  product: IProduct;
 
-  constructor() { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct(): any {
+    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
+      this.product = product;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
