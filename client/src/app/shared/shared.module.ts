3badeafb1ef9fcbd89c8643fded2f770bade9835
keith <xqk559@gmail.com>
2020-08-17 12:11:53 -0400

    Added ngx-bootstrap carousel of images as home page

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {PaginationModule} from 'ngx-bootstrap/pagination';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 
@@ -9,11 +10,13 @@ import { PagerComponent } from './components/pager/pager.component';
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
