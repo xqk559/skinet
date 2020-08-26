ac86602376d00052037a62da5260e52fb1261e8e
keith <xqk559@gmail.com>
2020-08-26 08:00:52 -0400

    Added register method to AccountController and corresponding RegisterDto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -35,5 +35,27 @@ namespace API.Controllers
             DisplayName = user.DisplayName
         };
     }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            UserName = registerDto.Email
+        };
+
+        var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+
+        return new UserDto
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            Token = "This will be a token"
+        };
+    }
   }
 }
