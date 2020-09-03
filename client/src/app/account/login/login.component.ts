9bc5d8d59a6b37a12158ed3b321c98899dddc85a
keith <xqk559@gmail.com>
2020-09-03 07:25:28 -0400

    added login() to onSubmit() in login.component so submitted login form data is used to log user in

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
     this.createLoginForm();
@@ -23,7 +24,11 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.loginForm.value);
+    this.accountService.login(this.loginForm.value).subscribe(() => {
+      console.log('user logged in');
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
