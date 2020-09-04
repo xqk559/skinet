da1f5ef4b25ba5d51218c8b24e9d24eca75f7fd4
keith <xqk559@gmail.com>
2020-09-04 07:59:31 -0400

    Made reusable text input form and used it for email and password login form

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -19,7 +19,8 @@ export class LoginComponent implements OnInit {
 
   createLoginForm(): any {
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
+      email: new FormControl('', [Validators.required, Validators
+        .pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
       password: new FormControl('', Validators.required)
     });
   }
