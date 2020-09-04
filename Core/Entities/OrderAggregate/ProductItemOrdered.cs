f1de2c906001d09cf22f81146df6cb003f2425a8
keith <xqk559@gmail.com>
2020-09-04 11:08:06 -0400

    Added Order entitites, configs, and seed data, then migrated that data into SQLite

--- Core/Entities/OrderAggregate/ProductItemOrdered.cs
+++ Core/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -2,18 +2,18 @@ namespace Core.Entities.OrderAggregate
 {
     public class ProductItemOrdered
     {
-    public ProductItemOrdered()
-    {
-    }
+        public ProductItemOrdered()
+        {
+        }
 
-    public ProductItemOrdered(int productItemId, string productName, string pictureUrl)
-    {
-      ProductItemId = productItemId;
-      ProductName = productName;
-      PictureUrl = pictureUrl;
-    }
+        public ProductItemOrdered(int productItemId, string productName, string pictureUrl)
+        {
+        ProductItemId = productItemId;
+        ProductName = productName;
+        PictureUrl = pictureUrl;
+        }
 
-    public int ProductItemId { get; set; }
+        public int ProductItemId { get; set; }
         public string ProductName { get; set; }
         public string PictureUrl { get; set; }
     }
--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -40,6 +41,21 @@ namespace Infrastructure.Data
           foreach (var item in types)
           {
             context.ProductTypes.Add(item);
+          }
+
+          await context.SaveChangesAsync();
+        }
+
+        if (!context.DeliveryMethods.Any())
+        {
+          var dmData =
+              File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+
+          var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
+
+          foreach (var item in methods)
+          {
+            context.DeliveryMethods.Add(item);
           }
 
           await context.SaveChangesAsync();
