b3e64f04c9a8def6c45fedd41d0f1e5d16d12dd3
keith <xqk559@gmail.com>
2020-09-09 08:22:15 -0400

    Added to OrderService.cs methods to get basket from repo, get items from product repo, get delivery method, calculate subtotal, create an order, and return it

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
 using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 
@@ -7,9 +9,47 @@ namespace Infrastructure.Services
 {
   public class OrderService : IOrderService
   {
-    public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
+    private readonly IGenericRepository<Order> _orderRepo;
+    private readonly IGenericRepository<Product> _productRepo;
+    private readonly IBasketRepository _basketRepo;
+    private readonly IGenericRepository<DeliveryMethod> _dmRepo;
+    public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo, IGenericRepository<Product> productRepo, IBasketRepository basketRepo)
     {
-      throw new System.NotImplementedException();
+      _dmRepo = dmRepo;
+      _basketRepo = basketRepo;
+      _productRepo = productRepo;
+      _orderRepo = orderRepo;
+    }
+
+    public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
+    {
+      // get basket from the repo
+      var basket = await _basketRepo.GetBasketAsync(basketId);
+
+      // get items from the product repo
+      var items = new List<OrderItem>();
+      foreach (var item in basket.Items)
+      {
+        var productItem = await _productRepo.GetByIdAsync(item.Id);
+        var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name,
+        productItem.PictureUrl);
+        var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+        items.Add(orderItem);
+      }
+
+      // get delivery method from repo
+      var deliveryMethod = await _dmRepo.GetByIdAsync(deliveryMethodId);
+
+      // calculate subtotal
+      var subtotal = items.Sum(items => items.Price * items.Quantity);
+
+      // create order
+      var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+
+      // TODO: save to db
+
+      // return order
+      return order;
     }
 
     public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodAsync()
