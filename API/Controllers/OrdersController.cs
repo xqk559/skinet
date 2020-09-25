af381b7bf832c89e6e5bdcf37ab32354668e7423
keith <xqk559@gmail.com>
2020-09-25 11:26:36 -0400

    Added Http methods to ORdersController

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -35,6 +36,34 @@ namespace API.Controllers
         if (order == null) return BadRequest(new ApiResponse(400, "Problem creating order"));
 
         return Ok(order);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Order>>> GetOrdersForUser()
+    {
+      var email = HttpContext.User.RetrieveEmailfromPrincipal();
+
+      var orders = await _orderService.GetOrdersForUserAsync(email);
+
+      return Ok(orders);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Order>> GetOrderByIdForUser(int id)
+    {
+      var email = HttpContext.User.RetrieveEmailfromPrincipal();
+
+      var order = await _orderService.GetOrderByIdAsync(id, email);
+
+      if (order == null) return NotFound(new ApiResponse(404));
+
+      return order;
+    }
+
+    [HttpGet("deliveryMethods")]
+    public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+    {
+      return Ok(await _orderService.GetDeliveryMethodAsync());
     }
   }
 }
