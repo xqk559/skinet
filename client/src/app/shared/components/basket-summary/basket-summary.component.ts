7c07250ed47e7153855098e45c1c775d2434e722
keith <xqk559@gmail.com>
2020-10-13 09:52:54 -0400

    MAde basket a shared component and built basket summary component

---
+++ client/src/app/shared/components/basket-summary/basket-summary.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasket, IBasketItem } from '../../models/basket';
+
+@Component({
+  selector: 'app-basket-summary',
+  templateUrl: './basket-summary.component.html',
+  styleUrls: ['./basket-summary.component.scss']
+})
+export class BasketSummaryComponent implements OnInit {
+  basket$: Observable<IBasket>;
+  @Output() decrement: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
+  @Output() increment: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
+  @Output() remove: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
+
+  constructor(private basketService: BasketService) { }
+
+  ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
+  }
+
+  decrementItemQuantity(item: IBasketItem): any {
+    this.decrement.emit(item);
+  }
+
+  incrementItemQuantity(item: IBasketItem): any {
+    this.increment.emit(item);
+  }
+
+  removeBasketItem(item: IBasketItem): any {
+    this.remove.emit(item);
+  }
+
+}
